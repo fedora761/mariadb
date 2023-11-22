bcab45f34829bac924d6e8981bae885e6fc643bc
root <root@git.myguest.virtualbox.org>
2023-11-22 15:56:25 +0900

    쉘 스크립트 수정

--- backup_test/mariadb_backup.sh
+++ backup_test/mariadb_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 datetime=$(date +"%y%m%d")
 
-mariabackup --backup --history --no-lock --user=root --password=xladjq --target-dir="/data/backup/full_backup_$datetime"
-#mariabackup --backup  --user=root --password=xladjq --target-dir="/data/backup/full_backup_$datetime"
+mariabackup --backup --history --no-lock --user=root --password=password --target-dir="/data/backup/full_backup_$datetime"
+#mariabackup --backup  --user=root --password=password --target-dir="/data/backup/full_backup_$datetime"
